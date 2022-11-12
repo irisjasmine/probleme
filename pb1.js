535caed9e3fe93e9ad55d014c27006fd48d43aa0
irisjasmine <102521337+irisjasmine@users.noreply.github.com>
2022-11-12 14:10:24 +0200

    Update pb1.js

--- pb1.js
+++ pb1.js
@@ -13,18 +13,22 @@ var day = today.getDay();
 var hour = today.getHours();
 var minute = today.getMinutes();
 var second = today.getSeconds();
-var period = hour >= 12 ? "PM" : "AM";
-hour = hour >= 12 ? hour - 12 : hour;
+var period = hour;
+if ( hour >= 12 ) { 
+  period = "PM";
+} else period = "AM";
 
-if (hour === 0 && period === " AM " && minute === 0 && second === 0) {
+if (hour >= 12) {
+  hour = hour - 12;
+} 
+
+if (hour === 0 && period === "AM" && minute === 0 && second === 0) {
     hour = 12;
-    period = " AM";
   }
 }
 
-if (hour === 0 && period === " PM " && minute === 0 && second === 0) {
+if (hour === 0 && period === "PM" && minute === 0 && second === 0) {
     hour = 12;
-    period = " PM";
   }
 }
 
